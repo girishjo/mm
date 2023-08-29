2e36c1844a5da792d1be7dfb170a697d1f642c56
Girish Joshi <girish_joshi@siemens.com>
2023-08-29 15:02:40 +0530

    data merge

--- scripts/data.js
+++ scripts/data.js
@@ -3,9 +3,18 @@ var bseData;
 var defaultStockList;
 
 window.onload = async () => {
-  let response = await fetch('./data/nsedata.json');
-  const nData = await response.json();
+  let response = await fetch('./data/nseDelivery.json');
+  let nData = await response.json();
   nseData = nData.data;
+  document.getElementById('nsedate').innerText += " " + nData.deliveryTimeStamp;
+  if (new Date(nData.timeStamp).getDate() != new Date().getDate()) {
+    document.getElementById('nsedate').style.background = 'red';
+  }
+
+  response = await fetch('./data/nseOpenClose.json');
+  nData = await response.json();
+  nseData = MergeRecursive(nseData, nData.data);
+
 
   response = await fetch('./data/bsedata.json');
   bseData = await response.json();
@@ -13,10 +22,6 @@ window.onload = async () => {
   response = await fetch('./data/defaultStockList.json');
   defaultStockList = await response.json();
 
-  document.getElementById('nsedate').innerText += " " + nData.timeStamp;
-  if (new Date(nData.timeStamp).getDate() != new Date().getDate()) {
-    document.getElementById('nsedate').style.background = 'red';
-  }
 
   response = await fetch('./data/datebse.json');
   var bseDate = await response.json();
@@ -27,3 +32,25 @@ window.onload = async () => {
 
   loadDataFromLocal();
 };
+
+function MergeRecursive(obj1, obj2) {
+
+  for (var p in obj2) {
+    try {
+      // Property in destination object set; update its value.
+      if (obj2[p].constructor == Object) {
+        obj1[p] = MergeRecursive(obj1[p], obj2[p]);
+
+      } else {
+        obj1[p] = obj2[p];
+
+      }
+
+    } catch (e) {
+      // Property in destination object not set; create it and set its value.
+      obj1[p] = obj2[p];
+
+    }
+  }
+  return obj1;
+}
