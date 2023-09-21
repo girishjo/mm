2cc910a1ed7b1b5e68d34126a9024125af5f3e8e
Girish Joshi <girish_joshi@siemens.com>
2023-09-21 23:16:01 +0530

    minor bug

--- dev/scripts/defaults.js
+++ dev/scripts/defaults.js
@@ -14,7 +14,7 @@ const todayDateHour = todayDate;
 todayDate = new Date(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate());
 
 window.addEventListener('load', async () => {
-    settings = await GetData('../data/' + 'settings.json');
+    settings = await GetData('./data/' + 'settings.json');
     defaultWatchlists = await GetData('../data/' + 'defaultWatchlists.json');
     if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
         LoadLocalDefaults()
