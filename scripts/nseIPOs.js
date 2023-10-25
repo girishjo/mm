eb56d002d852e51ab8f7f7df5b62746963737740
Girish Joshi <girish_joshi@siemens.com>
2023-10-26 02:20:23 +0530

    feat: uno new NSE IPOs test

--- scripts/nseIPOs.js
+++
@@ -1,7 +0,0 @@
-var newNseIpos = [];
-
-async function GetNseIPOs() {
-    let response = await fetch('https://www.nseindia.com/api/new-listing-today-ipo?index=NewListing');
-    let dataJson = await response.json();
-    newNseIpos.push(...dataJson.data);
-}
