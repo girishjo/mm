351a062e7d583c3298d6a69169add98a000ce810
Girish Joshi <girish_joshi@siemens.com>
2024-03-08 18:42:52 +0530

    fix: market holidays were picked from user's local machine

--- scripts/defaults.js
+++ scripts/defaults.js
@@ -170,11 +170,13 @@ function mergeDeep(target, ...sources) {
 
     if (isObject(target) && isObject(source)) {
         for (const key in source) {
-            if (isObject(source[key])) {
-                if (!target[key]) Object.assign(target, { [key]: {} });
-                mergeDeep(target[key], source[key]);
-            } else {
-                Object.assign(target, { [key]: source[key] });
+            if (key != "marketHolidays") {
+                if (isObject(source[key])) {
+                    if (!target[key]) Object.assign(target, { [key]: {} });
+                    mergeDeep(target[key], source[key]);
+                } else {
+                    Object.assign(target, { [key]: source[key] });
+                }
             }
         }
     }
